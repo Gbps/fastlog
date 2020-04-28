4439977c9ff13d63c20566bf5b5d4c1e91274c4c
Michael <gbps@ctf.re>
2020-04-28 15:11:11 -0400

    Windows compat for missing curses module

--- fastlog/termcap.py
+++ fastlog/termcap.py
@@ -1,18 +1,41 @@
 # Terminal features and colors
 # Relies on a termcap database contained within the curses package
 # pwnlib/term/termcap.py
-import curses
 import os
 
+_win_compat = None
+
+try:
+    import curses
+except ImportError:
+    _win_compat = {
+        "bold": {(): "\x1b\x5b\x31\x6d"},
+        "setaf": {
+            (1,): "\x1b\x5b\x33\x31\x6d",
+            (2,): "\x1b\x5b\x33\x32\x6d",
+            (3,): "\x1b\x5b\x33\x33\x6d",
+            (4,): "\x1b\x5b\x33\x34\x6d",
+            (5,): "\x1b\x5b\x33\x35\x6d",
+            (7,): "\x1b\x5b\x33\x37\x6d",
+        },
+        "setab": {(1,): "\x1b\x5b\x34\x31\x6d"},
+    }
+
 _cache = {}
+
 
 def get(cap, *args, **kwargs):
     """
     Get a terminal capability exposes through the `curses` module.
     """
     # Hack for readthedocs.org
-    if 'READTHEDOCS' in os.environ:
-        return ''
+    if "READTHEDOCS" in os.environ:
+        return ""
+
+    if _win_compat != None:
+        print(cap, args)
+        ret = _win_compat.get(cap).get(args)
+        return ret
 
     if kwargs != {}:
         raise TypeError("get(): No such argument %r" % kwargs.popitem()[0])
@@ -23,7 +46,7 @@ def get(cap, *args, **kwargs):
             curses.setupterm()
         except:
             pass
-    
+
     s = _cache.get(cap)
     if not s:
         s = curses.tigetstr(cap)
@@ -33,7 +56,7 @@ def get(cap, *args, **kwargs):
                 s = curses.tigetflag(cap)
                 if s == -1:
                     # default to empty string so tparm doesn't fail
-                    s = ''
+                    s = ""
                 else:
                     s = bool(s)
         _cache[cap] = s
@@ -41,9 +64,9 @@ def get(cap, *args, **kwargs):
     # if 's' is not set 'curses.tparm' will throw an error if given arguments
     if args and s:
         r = curses.tparm(s, *args)
-        return r.decode('utf-8')
+        return r.decode("utf-8")
     else:
         if isinstance(s, bytes):
-            return s.decode('utf-8')
+            return s.decode("utf-8")
         else:
             return s
